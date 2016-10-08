4629a05f895a6bf9898087f083c26d60304004ac
Michael Castillo Ocampo <mcastillo@asfi.gob.bo>
2016-10-08 10:11:46 -0400

    Comentario de prueba

---
+++ HelloWorld.java
@@ -0,0 +1,9 @@
+/*
+comentarios mpara multiples lines
+*/
+class HelloWorld{
+//comentario para una linea
+	public static void main(String args[]){
+			System.out.println("Hello from Java!!");
+		}
+	}
